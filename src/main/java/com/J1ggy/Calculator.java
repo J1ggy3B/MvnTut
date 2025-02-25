e756b1ba7c5880b5d98e5b4ab004481ec7731f55
J1ggy2B <watsongraeme418@mail.uk>
2025-02-25 14:56:02 +0000

    minor error fix

--- src/main/java/com/J1ggy/Calculator.java
+++ src/main/java/com/J1ggy/Calculator.java
@@ -10,8 +10,7 @@ public double multiply(double num1, double num2) {
 return num1 * num2; 
 } 
 public double divide(double num1, double num2) { 
-if (num2 == 0) throw new IllegalArgumentException("Division by zero: divisor 
-must not be 0"); 
+if (num2 == 0) throw new IllegalArgumentException("Division by zero: divisor must not be 0"); 
 return num1 / num2; 
 } 
 }
